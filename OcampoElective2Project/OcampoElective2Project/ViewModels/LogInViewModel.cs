bacb2b2dca4b151ed1f3e93cf5d0681584867ff9
mjiocampo <mjiocampo@JEELAB-005>
2018-12-11 12:26:33 +0800

    added navigation from loginpage to registratonp [age(still error) addedd ui in regisrationpage

--- OcampoElective2Project/OcampoElective2Project/ViewModels/LogInViewModel.cs
+++ OcampoElective2Project/OcampoElective2Project/ViewModels/LogInViewModel.cs
@@ -4,13 +4,14 @@ using System.Text;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Command;
 using OcampoElective2Project.Helpers;
+using Xamarin.Forms;
 
 namespace OcampoElective2Project.ViewModels
 {
     public class LogInViewModel : OcampoElective2ProjectViewModel
     {
 
-        public ICommand LoginCommand => new RelayCommand(LoginProc);
+        public ICommand LogInCommand => new RelayCommand(LoginProc);
         public void LoginProc()
         {
             //var user = LoginService.Check(Username, Password);
@@ -28,6 +29,13 @@ namespace OcampoElective2Project.ViewModels
                 //Username = "";
                 //Password = "";
             }
+
+        }
+        public ICommand RegisterCommand =>new Command(RegisterProc);
+
+        private void RegisterProc()
+        {
+            NavigationService.NavigateTo(ViewModelLocator.RegistrationPage);
         }
     }
 }
